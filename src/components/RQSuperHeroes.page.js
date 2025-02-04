ea2baa9261cb327a84c66c30ec08d6d226951842
Beaskyy <afeez.akintobi@yahoo.com>
2025-02-04 21:00:17 +0100

    refetch

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,18 +1,30 @@
 import axios from "axios";
 import { useQuery } from "react-query";
-
-const fetcherHeroes = () => {
-  return axios("http://localhost:4000/superheroes");
+const fetchSuperHeroes = async () => {
+  const {data } = await axios.get("http://localhost:4000/superheroes");
+  return data
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-heros", fetcherHeroes);
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes, {
+      enabled: false
+    }
+  );
 
-  if (isLoading) <h2>Loading...</h2>;
+  if (isLoading || isFetching) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>Error: {error.message}</h2>;
+  }
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
-      {data?.data.map((hero) => (
+      <button onClick={() => refetch()}>Refetch</button>
+      {data?.map((hero) => (
         <div key={hero.id}>{hero.name}</div>
       ))}
     </>
